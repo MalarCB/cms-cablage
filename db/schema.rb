b4d710764ca40f109b1e1d33cae95966655bd593
MalarCB <malarvizhy.soupramanien@careerbuilder.com>
2015-11-15 12:19:52 +0100

    re generate model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115110834) do
+ActiveRecord::Schema.define(version: 20151115111907) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "user"
